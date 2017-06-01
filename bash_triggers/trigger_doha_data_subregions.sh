97edd458cd11c9be190cb1dea656443ac68c23be
Matheus Araujo <maraujo@qf.org.qa>
2017-06-01 12:02:06 +0300

    Adding nyork_demographics

--- bash_triggers/trigger_doha_data_subregions.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python runDataCollection.py --json_input doha_demographics/doha_subregions.json --data_folder doha_demographics/data_folder --raw_output /tmp/raw_output_$RANDOM.csv
